ccd6078bb459ceeb7a74c7fba4697751f872d440
karlhadwen <karlhadwen@gmail.com>
2020-04-27 15:18:42 +0100

    feat: src

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
+
+server.listen(port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
+
